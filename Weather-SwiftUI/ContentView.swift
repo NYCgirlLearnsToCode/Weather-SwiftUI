e538577e8cf5ccee340df500f71493d6f2826340
Lisa <imnotlostrui@aim.com>
2025-05-22 15:44:26 -0400

    Add custom color and more intialUI

--- Weather-SwiftUI/ContentView.swift
+++ Weather-SwiftUI/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .white],
+            LinearGradient(colors: [.blue, Color("lightBlue")],
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
             .edgesIgnoringSafeArea(.all)
@@ -20,7 +20,7 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .padding()
                 
-                VStack(spacing: 8) {
+                VStack(spacing: 10) {
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
                         .resizable()
@@ -31,25 +31,39 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
                 }
-                
-                HStack {
-                    VStack(alignment: .center) {
-                        Text("TUE")
-                            .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.white)
-                        
-                        Image(systemName: "cloud.sun.fill")
-                            .renderingMode(.original)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40, height: 40)
-                        
-                        Text("74°")
-                            .foregroundColor(.white)
-                    }
-                }
-                .padding(.top)
+                .padding(.bottom, 40)
 
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temperature: 82)
+                    WeatherDayView(dayOfWeek: "THURS",
+                                   imageName: "cloud.drizzle.fill",
+                                   temperature: 76)
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "cloud.fog.fill",
+                                   temperature: 70)
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "sun.max.fill",
+                                   temperature: 80)
+                }
+
+                Spacer()
+                
+                Button {
+                    //action
+                    print("tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background()
+                        .font(.system(size: 20, weight: .bold))
+                        .cornerRadius(10)
+                }
+                
                 Spacer()
             }
         }
@@ -58,4 +72,30 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .foregroundColor(.white)
+                .font(.system(size: 28, weight: .medium))
+        }
+    }
 }
